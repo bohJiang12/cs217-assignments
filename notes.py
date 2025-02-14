b6cf2fdf16cb7203f98cbd9edcc4f5c5c079a274
Bohan Jiang <bohanj.cd@gmail.com>
2025-02-13 22:25:18 -0500

    added notebook class and test codes

---
+++ notes.py
@@ -0,0 +1,144 @@
+"""
+The module implements a note-taking app, and it supports following functions:
+    * Add a note into existing notebook
+    * List all existing notes
+    * Find a note by searching user-provided term(s)
+    * Display contents of a note from user-entered note's name
+
+TODO: List examples (usages)
+
+In general, this note-taking app (or say notebook) has the following scheme:
+---
+Notebook
+    |___ Note
+        |--- name
+        |--- contents
+---
+"""
+from multiprocessing.connection import Listener
+from typing import Dict, List
+
+import pickle as pkl
+from pathlib import Path
+
+from nltk.tokenize import RegexpTokenizer
+
+
+CACHE_DIR = Path.cwd() / '.cache'
+DEFAULT_FILE = Path('recent_notes.pkl')
+
+class Notebook:
+    """Notebook class containing `Note` objects"""
+    def __init__(self):
+        self._notes: Dict[str, Note] = self._load_cache_notes()
+        self._inv_notes: Dict[str, str] = {note.text(): name for name, note in self._notes.items()}
+
+    def __setitem__(self, note_name, new_contents):
+        self._notes[note_name].update(new_contents)
+        self._auto_save()
+
+    def __getitem__(self, note_name: str):
+        """Retrieve `Note` object by its name as string"""
+        return self._notes[note_name]
+
+    def _load_cache_notes(self):
+        """
+        Check and load notes from last session
+        """
+        # Check if the cache directory exists first,
+        # then check if there's any cached notes from last session
+        if CACHE_DIR.exists():
+            cached_notes = CACHE_DIR / DEFAULT_FILE
+            try:
+                with open(cached_notes, 'rb') as f:
+                    return pkl.load(f)
+            except FileNotFoundError:
+                return {}
+
+        # Otherwise, create a cache directory
+        CACHE_DIR.mkdir()
+
+        return {}
+
+    def _auto_save(self):
+        """
+        Auto-save the notebook into cached file at real time
+        ---
+        The function will only be called when
+            * new note is added
+            * existing note is updated
+        """
+        assert self._notes is not None
+
+        with open(CACHE_DIR / DEFAULT_FILE, 'wb') as f:
+            # noinspection PyTypeChecker
+            pkl.dump(self._notes, f)
+
+    def notes(self):
+        """Return names of existing notes"""
+        return set(self._notes.keys())
+
+    def add(self, name: str, text: str) -> bool:
+        """
+        Add new notes with given name and text
+
+        >>> nb = Notebook()
+        >>> nb.add('nlp', 'system')
+        True
+        """
+        self._notes.update({name: Note(name, text)})
+        self._inv_notes.update({text: name})
+        self._auto_save()
+
+        return True
+
+    def find(self, term: str) -> List[str]:
+        """
+        Find a `Note` object from the provided term
+
+        Usage:
+        ---
+        Given the notebook has only one note, which is {name: Wed, text: It's a rainy day.}
+
+        >>> nb = Notebook()
+        >>> nb.add(name='Wed', text="It's a rainy day.")
+        True
+        >>> nb.find('rainy')
+        ['Wed']
+        >>> nb.find('sunny')
+        []
+
+        :param term: user-provided term that might be in a note
+        :return: a list of `Note` objects that contains provided term
+        """
+
+        results = []
+        tokenizer = RegexpTokenizer(r'\w+')
+
+        for text, name in self._inv_notes.items():
+            bow = set(tokenizer.tokenize(text.lower()))
+            if term in bow:
+                results.append(name)
+
+        return results
+
+
+
+class Note:
+    """Note class consisting of `name` and `contents` attributes"""
+
+    def __init__(self, name: str, contents: str):
+        self._name = name
+        self._text = contents
+
+    def text(self) -> str:
+        """Return the contents of the note"""
+        return self._text
+
+    def name(self):
+        """Return the name of the note"""
+        return self._name
+
+    def update(self, new_text: str):
+        """Update note's contents"""
+        self._text = new_text
---
+++ test/test_nb.py
@@ -0,0 +1,88 @@
+import unittest
+
+import os
+import time
+
+from notes import (
+    Note,
+    Notebook,
+    DEFAULT_FILE,
+    CACHE_DIR
+)
+
+class TestNote(unittest.TestCase):
+    """Test `Note` class"""
+
+    def setUp(self):
+        self.true_name, self.true_text = 'nlp', 'system'
+        self.note = Note(self.true_name, self.true_text)
+
+    def test_init_note(self):
+        self.assertEqual(self.true_name, self.note.name())
+        self.assertEqual(self.true_text, self.note.text())
+
+    def test_update(self):
+        new_note = 'statistics'
+        self.note.update(new_note)
+        self.assertEqual(self.true_name, self.note.name())
+        self.assertEqual(new_note, self.note.text())
+
+
+class TestNotebook(unittest.TestCase):
+    """Test `Notebook` class"""
+
+    def setUp(self):
+        if CACHE_DIR.exists() and any(CACHE_DIR.iterdir()):
+            for file in CACHE_DIR.glob('*'):
+                if file.is_file():
+                    file.unlink()
+
+        self.nb = Notebook()
+
+    def test_init_nb(self):
+        """
+        To test:
+            * if `.cache` directory is created
+            * if there's no note added
+        """
+        self.assertTrue(CACHE_DIR.exists())
+        self.assertEqual(0, len(self.nb.notes()))
+
+    def test_add_note(self):
+        """
+        To test the function of adding notes:
+            * `.cache` directory has modified `.pkl` file
+            * length of notebook is enlarged
+        """
+        new_name, new_text = 'Wed', 'No class today.'
+
+        # Test the very first add
+        self.nb.add(new_name, new_text)
+        self.assertEqual(1, len(self.nb.notes()))
+
+        # Test the second add
+        second_name, second_text = 'Fri', 'Tomorrow is weekend.'
+        self.nb.add(second_name, second_text)
+
+        self.assertEqual(2, len(self.nb.notes()))
+
+    def test_search_notes(self):
+        """
+        To test the function for search notes given a term that might be in the note
+        """
+        self.nb.add('1', 'Today is Friday.')
+        self.nb.add('2', 'I passed my exam!')
+        self.nb.add('3', "It's Friday, but I have work to do.")
+
+        self.assertEqual(['1', '3'], self.nb.find('friday'))
+        self.assertEqual(['2'], self.nb.find('exam'))
+        self.assertEqual([], self.nb.find('Monday'))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
